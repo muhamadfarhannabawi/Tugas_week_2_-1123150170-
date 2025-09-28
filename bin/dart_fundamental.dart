c70f152eebe52e1352e9095f2f84be2c9ea4fc2e
muhamadfarhannabawi <1123150170@global.ac.id>
2025-09-28 19:45:03 +0700

    first commit kalkulator sederhana

---
+++ bin/dart_fundamental.dart
@@ -0,0 +1,47 @@
+import 'dart:io';
+
+double _readNumber(String label) {
+while (true) {
+stdout.write('$label: ');
+final input = stdin.readLineSync();
+if (input == null) continue;
+final value = double.tryParse(input.trim());
+if (value != null) return value;
+print('Input tidak valid. Masukkan angka');
+}
+}
+
+void main() {
+while (true) {
+print('\n=== Kalkulator barcelona ===');
+print('1) Tambah');
+print('2) Kurang');
+print('3) Kali');
+print('4) Bagi');
+print('5) Keluar');
+stdout.write('Pilih [1-5]: ');
+final pilih = stdin.readLineSync()?.trim();
+if (pilih == '5') {
+print('Bye!');
+return;
+}
+final a = _readNumber('Masukkan angka pertama');
+final b = _readNumber('Masukkan angka kedua');
+switch (pilih) {
+case '1':
+print('Hasil: ${a + b}');
+break;
+case '2':
+print('Hasil: ${a - b}');
+break;
+case '3':
+print('Hasil: ${a * b}');
+break;
+case '4':
+print('Hasil: ${a / b}');
+break;
+default:
+print('Pilihan tidak dikenal.');
+}
+}
+}
